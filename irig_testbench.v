dd5d30811dd25fb692554eafd811924dec6a7529
John Kelley <jkelley@icecube.wisc.edu>
2016-02-17 16:57:10 -0600

    Fix clock period and irig hold time

--- irig_testbench.v
+++ irig_testbench.v
@@ -6,20 +6,22 @@ module irig_testbench();
     localparam D1 = 3'b010;
     localparam D0 = 3'b001;
 
-    // Instantiate the DUT
-    irig i1(.clk_10mhz(clk_10mhz),
-            .irigb(irigb),
-            .pps(pps),
-            .rst(rst));
-    
+
     // Inputs to the DUT
     reg clk_10mhz;
     reg rst;
-    reg irig_b;
-    
+    reg irigb;
+ 
     // Output of the DUT
     wire pps;
     
+    
+    // Instantiate the DUT
+    irig i1(.clk_10mhz(clk_10mhz),
+            .irigb(irigb),
+            .pps(pps),         
+            .rst(rst));
+            
     // Reset
     initial begin
         clk_10mhz = 1'b0;
@@ -50,7 +52,7 @@ module irig_testbench();
 end
 
 always
-  #100 clk_10mhz = ~clk_10mhz;
+  #50 clk_10mhz = ~clk_10mhz;
 
 task irig_bit;
     input [2:0] ib; // mark, 1, 0
@@ -58,10 +60,23 @@ task irig_bit;
         @(posedge clk_10mhz);
         irigb = 1'b1;
         case (ib)
-          D0:   #2000500 irigb = 1'b0;
-          D1:   #5000500 irigb = 1'b0;
-          MARK: #8000500 irigb = 1'b0;
+          D0: 
+            begin
+              #2000500 irigb = 1'b0;
+              #7999500;
+            end
+          D1:
+            begin
+              #5000500 irigb = 1'b0;
+              #4999500;
+            end
+          MARK:
+            begin 
+              #8000500 irigb = 1'b0;
+              #2999500;
+            end
         endcase            
     end
+endtask
 
 endmodule
