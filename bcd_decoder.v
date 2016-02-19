7a35771c83043682e9d294ab1865a5b690497e07
John Kelley <jkelley@icecube.wisc.edu>
2016-02-19 16:15:52 -0600

    Added BCD timestamp decoders

--- bcd_decoder.v
+++ bcd_decoder.v
@@ -1,15 +1,15 @@
 module bcd_decoder (
-                    input [2:0] bcd_bit_idx ,
-                    input [1:0] bcd_digit_idx ,
-                    input  bcd_bit,
+                    input [2:0]      bcd_bit_idx ,
+                    input [1:0]      bcd_digit_idx ,
+                    input            bcd_bit,
                     output [8:0] value);
 
     // Combinatorial BCD decoder
     //   bcd_bit_idx: index of binary bit in the BCD digit
     //   bcd_digit_idx: index of digit (i.e. power of 10)
     
-    // Returns value to be added to decoded sum
     reg [6:0]                    bcd_multiplier;
+    
     always @(*) begin
         case (bcd_digit_idx)
           2'd0:
--- irig.v
+++ irig.v
@@ -1,10 +1,13 @@
 module irig(input clk_10mhz,
-				input  irigb,
-            output pps,
-            input rst);
+			input         irigb,
+            output        pps,
+            output [8:0]  ts_day,
+            output [6:0]  ts_year,
+            output [16:0] ts_sec_day,
+            input         rst);
     
     wire           irig_d0, irig_d1, irig_mark;
-    
+
     irig_width_decode id(.clk(clk_10mhz),
                          .irigb(irigb),
                          .irig_mark(irig_mark),
@@ -17,6 +20,9 @@ module irig(input clk_10mhz,
                       .irig_d1(irig_d1),
                       .irig_mark(irig_mark),
                       .pps(pps),
+                      .ts_day(ts_day),
+                      .ts_year(ts_year),
+                      .ts_sec_day(ts_sec_day),
                       .rst(rst));
    
 endmodule
