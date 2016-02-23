7b733fbe112934eed4626b4431cddbdfe0162edf
John Kelley <jkelley@icecube.wisc.edu>
2016-02-23 11:45:58 -0600

    Untabify

--- bcd_decoder.v
+++ bcd_decoder.v
@@ -1,28 +1,28 @@
 module bcd_decoder (
-					input [2:0]		 bcd_bit_idx ,
-					input [1:0]		 bcd_digit_idx ,
-					input			 bcd_bit,
-					output [8:0] value);
+                    input [2:0]      bcd_bit_idx ,
+                    input [1:0]      bcd_digit_idx ,
+                    input            bcd_bit,
+                    output [8:0] value);
 
-	// Combinatorial BCD decoder
-	//	 bcd_bit_idx: index of binary bit in the BCD digit
-	//	 bcd_digit_idx: index of digit (i.e. power of 10)
-	
-	reg [6:0]					 bcd_multiplier;
-	
-	always @(*) begin
-		case (bcd_digit_idx)
-		  2'd0:
-			bcd_multiplier = 7'd1;
-		  2'd1:
-			bcd_multiplier = 7'd10;
-		  2'd2:
-			bcd_multiplier = 7'd100;
-		  default:
-			bcd_multiplier = 7'd0;
-		  endcase
-	end
+    // Combinatorial BCD decoder
+    //   bcd_bit_idx: index of binary bit in the BCD digit
+    //   bcd_digit_idx: index of digit (i.e. power of 10)
+    
+    reg [6:0]                    bcd_multiplier;
+    
+    always @(*) begin
+        case (bcd_digit_idx)
+          2'd0:
+            bcd_multiplier = 7'd1;
+          2'd1:
+            bcd_multiplier = 7'd10;
+          2'd2:
+            bcd_multiplier = 7'd100;
+          default:
+            bcd_multiplier = 7'd0;
+          endcase
+    end
 
-	assign value = (bcd_bit << bcd_bit_idx) * bcd_multiplier;
+    assign value = (bcd_bit << bcd_bit_idx) * bcd_multiplier;
 
 endmodule
--- irig_width_decode.v
+++ irig_width_decode.v
@@ -1,43 +1,43 @@
 module irig_width_decode (
-						  input		 clk,
-						  input		 irigb,
-						  output reg irig_mark,
-						  output reg irig_d0,
-						  output reg irig_d1,
-						  input		 rst
-						  );
-	
-	
-	// 10MHz clock and 10kHz IRIG-B
-	// Width encoding of the three states
-	localparam CYCLES_ZERO = 17'd20000;
-	localparam CYCLES_ONE  = 17'd50000;
-	localparam CYCLES_MARK = 17'd80000;
-	
-	// Clock cycles in an IRIG bit
-	reg [16:0]						 clk_cnt = 17'b0;
-	reg								 irigb_last = 1'b0;
-	
-	always @(posedge clk) begin
-		if (rst) begin
-			clk_cnt <= 17'b0;
-			irigb_last = 1'b0;
-			irig_d0 <= 1'b0;
-			irig_d1 <= 1'b0;
-			irig_mark <= 1'b0;
-		end else begin
-			// Check widths at irig falling edge and produce one-cycle pulse
-			irig_mark <= (clk_cnt >= CYCLES_MARK) && !irigb && irigb_last && !irig_mark;
-			irig_d0	  <= (clk_cnt >= CYCLES_ONE) && (clk_cnt < CYCLES_MARK) && !irigb && irigb_last && !irig_d0;
-			irig_d1	  <= (clk_cnt >= CYCLES_ZERO) && (clk_cnt < CYCLES_ONE) && !irigb && irigb_last && !irig_d1;
-			
-			// Reset count on rising edge of irig bit
-			if (irigb && !irigb_last)
-			  clk_cnt <= 17'b0;
-			else
-			  clk_cnt <= clk_cnt+17'b1;
-			irigb_last <= irigb;
-		end
-	end
-	
+                          input      clk,
+                          input      irigb,
+                          output reg irig_mark,
+                          output reg irig_d0,
+                          output reg irig_d1,
+                          input      rst
+                          );
+    
+    
+    // 10MHz clock and 10kHz IRIG-B
+    // Width encoding of the three states
+    localparam CYCLES_ZERO = 17'd20000;
+    localparam CYCLES_ONE  = 17'd50000;
+    localparam CYCLES_MARK = 17'd80000;
+    
+    // Clock cycles in an IRIG bit
+    reg [16:0]                       clk_cnt = 17'b0;
+    reg                              irigb_last = 1'b0;
+    
+    always @(posedge clk) begin
+        if (rst) begin
+            clk_cnt <= 17'b0;
+            irigb_last = 1'b0;
+            irig_d0 <= 1'b0;
+            irig_d1 <= 1'b0;
+            irig_mark <= 1'b0;
+        end else begin
+            // Check widths at irig falling edge and produce one-cycle pulse
+            irig_mark <= (clk_cnt >= CYCLES_MARK) && !irigb && irigb_last && !irig_mark;
+            irig_d0   <= (clk_cnt >= CYCLES_ONE) && (clk_cnt < CYCLES_MARK) && !irigb && irigb_last && !irig_d0;
+            irig_d1   <= (clk_cnt >= CYCLES_ZERO) && (clk_cnt < CYCLES_ONE) && !irigb && irigb_last && !irig_d1;
+            
+            // Reset count on rising edge of irig bit
+            if (irigb && !irigb_last)
+              clk_cnt <= 17'b0;
+            else
+              clk_cnt <= clk_cnt+17'b1;
+            irigb_last <= irigb;
+        end
+    end
+    
 endmodule
