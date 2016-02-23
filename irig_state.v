f6006b7e28232d352508dde6ee5ff80d95204988
John Kelley <jkelley@icecube.wisc.edu>
2016-02-23 11:43:37 -0600

    Tabify

--- irig_state.v
+++ irig_state.v
@@ -1,175 +1,175 @@
 module irig_state (
-	               input            clk,
-                   input            rst,
-	               input            irig_d0,
-	               input            irig_d1,
-	               input            irig_mark,
-	               output reg       pps_gate, 
-                   output reg       ts_finish,
-                   output reg [2:0] ts_select,
-                   output reg [4:0] bit_idx,
-                   output reg [1:0] digit_idx,
-                   output reg       bit_value);
+				   input			clk,
+				   input			rst,
+				   input			irig_d0,
+				   input			irig_d1,
+				   input			irig_mark,
+				   output reg		pps_gate, 
+				   output reg		ts_finish,
+				   output reg [2:0] ts_select,
+				   output reg [4:0] bit_idx,
+				   output reg [1:0] digit_idx,
+				   output reg		bit_value);
 
-    // State machine states
-    localparam ST_UNLOCKED = 4'd0,
-      ST_PRELOCK  = 4'b1,
-      ST_START    = 4'd2,
-      ST_SECOND   = 4'd3,
-      ST_MINUTE   = 4'd4,
-      ST_HOUR     = 4'd5,
-      ST_DAY      = 4'd6,
-      ST_DAY2     = 4'd7,
-      ST_YEAR     = 4'd8,
-      ST_UNUSED1  = 4'd9,
-      ST_UNUSED2  = 4'd10,
-      ST_SEC_DAY  = 4'd11,
-      ST_SEC_DAY2 = 4'd12;
+	// State machine states
+	localparam ST_UNLOCKED = 4'd0,
+	  ST_PRELOCK  = 4'b1,
+	  ST_START	  = 4'd2,
+	  ST_SECOND	  = 4'd3,
+	  ST_MINUTE	  = 4'd4,
+	  ST_HOUR	  = 4'd5,
+	  ST_DAY	  = 4'd6,
+	  ST_DAY2	  = 4'd7,
+	  ST_YEAR	  = 4'd8,
+	  ST_UNUSED1  = 4'd9,
+	  ST_UNUSED2  = 4'd10,
+	  ST_SEC_DAY  = 4'd11,
+	  ST_SEC_DAY2 = 4'd12;
 
-    // Timestamp selection
-    localparam TS_SELECT_SECOND = 3'd1,
-      TS_SELECT_MINUTE = 3'd2,
-      TS_SELECT_HOUR = 3'd3,
-      TS_SELECT_DAY = 3'd4,
-      TS_SELECT_YEAR = 3'd5,
-      TS_SELECT_SEC_DAY = 3'd6;
+	// Timestamp selection
+	localparam TS_SELECT_SECOND = 3'd1,
+	  TS_SELECT_MINUTE = 3'd2,
+	  TS_SELECT_HOUR = 3'd3,
+	  TS_SELECT_DAY = 3'd4,
+	  TS_SELECT_YEAR = 3'd5,
+	  TS_SELECT_SEC_DAY = 3'd6;
 
-    // Count of the IRIG bits within a state
-    reg [3:0]                       irig_cnt;
+	// Count of the IRIG bits within a state
+	reg [3:0]						irig_cnt;
 
-    // PPS generation internal signal
-    // Output is registered version
-    reg                             pps_en;
-    
-    // Current and next state machine state
-    reg [3:0]                       state, next_state;
+	// PPS generation internal signal
+	// Output is registered version
+	reg								pps_en;
+	
+	// Current and next state machine state
+	reg [3:0]						state, next_state;
 
-    // Registers
-    always @(posedge clk) begin
-	    if (rst) begin
-		    state <= ST_UNLOCKED;
-            pps_gate <= 1'b0;
-            irig_cnt <= 4'b0;
-        end
-	    else begin
-		    state <= next_state;
-            pps_gate <= pps_en;
+	// Registers
+	always @(posedge clk) begin
+		if (rst) begin
+			state <= ST_UNLOCKED;
+			pps_gate <= 1'b0;
+			irig_cnt <= 4'b0;
+		end
+		else begin
+			state <= next_state;
+			pps_gate <= pps_en;
 
-            // Count the IRIG bits received between every MARK
-            if (irig_mark)
-              irig_cnt <= 4'b0;
-            else 
-              irig_cnt <= irig_cnt + (irig_d0 | irig_d1);
-        end
-    end
+			// Count the IRIG bits received between every MARK
+			if (irig_mark)
+			  irig_cnt <= 4'b0;
+			else 
+			  irig_cnt <= irig_cnt + (irig_d0 | irig_d1);
+		end
+	end
 
-    // IRIG decoding state machine
-    // FIX ME add checks that cause loss of lock
-    always @(*) begin
-        next_state = state;
-        pps_en = 1'b0;
-        ts_finish = 1'b0;
-        ts_select = 3'b0;
+	// IRIG decoding state machine
+	// FIX ME add checks that cause loss of lock
+	always @(*) begin
+		next_state = state;
+		pps_en = 1'b0;
+		ts_finish = 1'b0;
+		ts_select = 3'b0;
 		  bit_idx = 4'b0;
 		  digit_idx = 2'b0;
 		  bit_value = 1'b0;
-	    case (state)
-	      ST_UNLOCKED: begin
-		      if (irig_mark)
-			    next_state = ST_PRELOCK;
-	      end
-          ST_PRELOCK: begin
-		      if (irig_mark)
-			    next_state = ST_SECOND;
-		      else if (irig_d0 || irig_d1)
-			    next_state = ST_UNLOCKED;          
-          end
-	      ST_START: begin              
-              pps_en = 1'b1;
-		      if (irig_mark) begin
+		case (state)
+		  ST_UNLOCKED: begin
+			  if (irig_mark)
+				next_state = ST_PRELOCK;
+		  end
+		  ST_PRELOCK: begin
+			  if (irig_mark)
+				next_state = ST_SECOND;
+			  else if (irig_d0 || irig_d1)
+				next_state = ST_UNLOCKED;		   
+		  end
+		  ST_START: begin			   
+			  pps_en = 1'b1;
+			  if (irig_mark) begin
 				  next_state = ST_SECOND;
-              end
-	      end
-	      ST_SECOND: begin
-              ts_select = TS_SELECT_SECOND;
-              bit_idx = (irig_cnt > 4'd4) ? irig_cnt-4'd5 : irig_cnt;
-              digit_idx = (irig_cnt > 4'd4) ? 2'b1 : 2'b0;
-              bit_value = irig_d1 && !(irig_cnt == 4'd4);                
+			  end
+		  end
+		  ST_SECOND: begin
+			  ts_select = TS_SELECT_SECOND;
+			  bit_idx = (irig_cnt > 4'd4) ? irig_cnt-4'd5 : irig_cnt;
+			  digit_idx = (irig_cnt > 4'd4) ? 2'b1 : 2'b0;
+			  bit_value = irig_d1 && !(irig_cnt == 4'd4);				 
 
-		      if (irig_mark)
-			    next_state = ST_MINUTE;
-	      end
-	      ST_MINUTE: begin
-              ts_select = TS_SELECT_MINUTE;
-              bit_idx = (irig_cnt > 4'd4) ? irig_cnt-4'd5 : irig_cnt;
-              digit_idx = (irig_cnt > 4'd4) ? 2'b1 : 2'b0;
-              bit_value = irig_d1 && !(irig_cnt == 4'd4) && !(irig_cnt == 4'd8);
+			  if (irig_mark)
+				next_state = ST_MINUTE;
+		  end
+		  ST_MINUTE: begin
+			  ts_select = TS_SELECT_MINUTE;
+			  bit_idx = (irig_cnt > 4'd4) ? irig_cnt-4'd5 : irig_cnt;
+			  digit_idx = (irig_cnt > 4'd4) ? 2'b1 : 2'b0;
+			  bit_value = irig_d1 && !(irig_cnt == 4'd4) && !(irig_cnt == 4'd8);
 
-		      if (irig_mark)
-			    next_state = ST_HOUR;
-	      end		
-	      ST_HOUR: begin
-              ts_select = TS_SELECT_HOUR;
-              bit_idx = (irig_cnt > 4'd4) ? irig_cnt-4'd5 : irig_cnt;
-              digit_idx = (irig_cnt > 4'd4) ? 2'b1 : 2'b0;
-              bit_value = irig_d1 && !(irig_cnt == 4'd4) && !(irig_cnt >= 4'd8);
+			  if (irig_mark)
+				next_state = ST_HOUR;
+		  end		
+		  ST_HOUR: begin
+			  ts_select = TS_SELECT_HOUR;
+			  bit_idx = (irig_cnt > 4'd4) ? irig_cnt-4'd5 : irig_cnt;
+			  digit_idx = (irig_cnt > 4'd4) ? 2'b1 : 2'b0;
+			  bit_value = irig_d1 && !(irig_cnt == 4'd4) && !(irig_cnt >= 4'd8);
 
-		      if (irig_mark)
-			    next_state = ST_DAY;
-	      end
-	      ST_DAY: begin
-              ts_select = TS_SELECT_DAY;
-              bit_idx = (irig_cnt > 4'd4) ? irig_cnt-4'd5 : irig_cnt;
-              digit_idx = (irig_cnt > 4'd4) ? 2'd1 : 2'd0;
-              bit_value = irig_d1 && !(irig_cnt == 4'd4);
+			  if (irig_mark)
+				next_state = ST_DAY;
+		  end
+		  ST_DAY: begin
+			  ts_select = TS_SELECT_DAY;
+			  bit_idx = (irig_cnt > 4'd4) ? irig_cnt-4'd5 : irig_cnt;
+			  digit_idx = (irig_cnt > 4'd4) ? 2'd1 : 2'd0;
+			  bit_value = irig_d1 && !(irig_cnt == 4'd4);
 
-		      if (irig_mark)
-			    next_state = ST_DAY2;
-	      end
-	      ST_DAY2: begin
-              ts_select = TS_SELECT_DAY;
-              bit_idx = irig_cnt;
-              digit_idx = 2'd2;
-              bit_value = irig_d1 && !(irig_cnt > 4'd1);
+			  if (irig_mark)
+				next_state = ST_DAY2;
+		  end
+		  ST_DAY2: begin
+			  ts_select = TS_SELECT_DAY;
+			  bit_idx = irig_cnt;
+			  digit_idx = 2'd2;
+			  bit_value = irig_d1 && !(irig_cnt > 4'd1);
 
-		      if (irig_mark)
-			    next_state = ST_YEAR;
-	      end
-	      ST_YEAR: begin
-              ts_select = TS_SELECT_YEAR;
-              bit_idx = (irig_cnt > 4'd4) ? irig_cnt-4'd5 : irig_cnt;
-              digit_idx = (irig_cnt > 4'd4) ? 2'd1 : 2'd0;
-              bit_value = irig_d1 && !(irig_cnt == 4'd4);
+			  if (irig_mark)
+				next_state = ST_YEAR;
+		  end
+		  ST_YEAR: begin
+			  ts_select = TS_SELECT_YEAR;
+			  bit_idx = (irig_cnt > 4'd4) ? irig_cnt-4'd5 : irig_cnt;
+			  digit_idx = (irig_cnt > 4'd4) ? 2'd1 : 2'd0;
+			  bit_value = irig_d1 && !(irig_cnt == 4'd4);
 
-		      if (irig_mark)
-			    next_state = ST_UNUSED1;
-	      end
-	      ST_UNUSED1: begin
-		      if (irig_mark)
-			    next_state = ST_UNUSED2;
-	      end
-	      ST_UNUSED2: begin
-		      if (irig_mark)
-			    next_state = ST_SEC_DAY;
-	      end
-	      ST_SEC_DAY: begin
-              ts_select = TS_SELECT_SEC_DAY;
-              bit_idx = irig_cnt;
-              bit_value = irig_d1;
-		      if (irig_mark)
-			    next_state = ST_SEC_DAY2;
-	      end
-	      ST_SEC_DAY2: begin
-              ts_select = TS_SELECT_SEC_DAY;
-              bit_idx = irig_cnt+5'd9;
-              bit_value = irig_d1;
-		      if (irig_mark) begin
-			      next_state = ST_START;
-                  pps_en = 1'b1;
-                  ts_finish = 1'b1;
-              end
-	      end
-	    endcase
-    end
-    
+			  if (irig_mark)
+				next_state = ST_UNUSED1;
+		  end
+		  ST_UNUSED1: begin
+			  if (irig_mark)
+				next_state = ST_UNUSED2;
+		  end
+		  ST_UNUSED2: begin
+			  if (irig_mark)
+				next_state = ST_SEC_DAY;
+		  end
+		  ST_SEC_DAY: begin
+			  ts_select = TS_SELECT_SEC_DAY;
+			  bit_idx = irig_cnt;
+			  bit_value = irig_d1;
+			  if (irig_mark)
+				next_state = ST_SEC_DAY2;
+		  end
+		  ST_SEC_DAY2: begin
+			  ts_select = TS_SELECT_SEC_DAY;
+			  bit_idx = irig_cnt+5'd9;
+			  bit_value = irig_d1;
+			  if (irig_mark) begin
+				  next_state = ST_START;
+				  pps_en = 1'b1;
+				  ts_finish = 1'b1;
+			  end
+		  end
+		endcase
+	end
+	
 endmodule
--- irig_testbench.v
+++ irig_testbench.v
@@ -1,157 +1,157 @@
 `timescale 1 ns / 100 ps
 
 module irig_testbench();
-    
-    localparam MARK = 3'b100;
-    localparam D1 = 3'b010;
-    localparam D0 = 3'b001;
+	
+	localparam MARK = 3'b100;
+	localparam D1 = 3'b010;
+	localparam D0 = 3'b001;
 
-    // Inputs to the DUT
-    reg clk_10mhz;
-    reg rst;
-    reg irigb;
+	// Inputs to the DUT
+	reg clk_10mhz;
+	reg rst;
+	reg irigb;
  
-    // Output of the DUT
-    wire pps;    
-    wire [5:0] ts_second;
-    wire [5:0] ts_minute;
-    wire [4:0] ts_hour;
-    wire [8:0] ts_day;
-    wire [6:0] ts_year;
-    wire [16:0] ts_sec_day;
-    
-    // Instantiate the DUT
-    irig i1(.clk_10mhz(clk_10mhz),
-            .rst(rst),
-            .irigb(irigb),
-            .pps(pps),
-            .ts_second(ts_second),
-            .ts_minute(ts_minute),
-            .ts_hour(ts_hour),
-            .ts_day(ts_day),
-            .ts_year(ts_year),
-            .ts_sec_day(ts_sec_day));
-            
-    // Reset
-    initial begin
-        clk_10mhz = 1'b0;
-        irigb = 1'b0;
-        rst = 1'b1;
-        
-        // Reset goes low
-        #120 rst = 1'b0;        
-        #100;
+	// Output of the DUT
+	wire pps;	 
+	wire [5:0] ts_second;
+	wire [5:0] ts_minute;
+	wire [4:0] ts_hour;
+	wire [8:0] ts_day;
+	wire [6:0] ts_year;
+	wire [16:0] ts_sec_day;
+	
+	// Instantiate the DUT
+	irig i1(.clk_10mhz(clk_10mhz),
+			.rst(rst),
+			.irigb(irigb),
+			.pps(pps),
+			.ts_second(ts_second),
+			.ts_minute(ts_minute),
+			.ts_hour(ts_hour),
+			.ts_day(ts_day),
+			.ts_year(ts_year),
+			.ts_sec_day(ts_sec_day));
+			
+	// Reset
+	initial begin
+		clk_10mhz = 1'b0;
+		irigb = 1'b0;
+		rst = 1'b1;
+		
+		// Reset goes low
+		#120 rst = 1'b0;		
+		#100;
 
-        // Some garbage bits
-        irig_bit(D0);
-        irig_bit(MARK);
-        irig_bit(D1);
-        irig_bit(D0);
+		// Some garbage bits
+		irig_bit(D0);
+		irig_bit(MARK);
+		irig_bit(D1);
+		irig_bit(D0);
 
-        // End of second
-        irig_bit(MARK);
+		// End of second
+		irig_bit(MARK);
 
-        // Now send a full stream
-        irig_timestamp();
+		// Now send a full stream
+		irig_timestamp();
 
-        // Start of next one...
-        irig_bit(MARK);
+		// Start of next one...
+		irig_bit(MARK);
 
-        $stop;
-    end
+		$stop;
+	end
 
-    always
-      #50 clk_10mhz = ~clk_10mhz;
+	always
+	  #50 clk_10mhz = ~clk_10mhz;
 
-    // Send a full timestamp
-    task irig_timestamp;        
-        begin
-            // Frame identifier
-            irig_bit(MARK); // 00
+	// Send a full timestamp
+	task irig_timestamp;		
+		begin
+			// Frame identifier
+			irig_bit(MARK); // 00
 
-            // Seconds: 42 = 100_0010
-            // only 8 bits here
-            irig_bitstream(9'bx10000010);
+			// Seconds: 42 = 100_0010
+			// only 8 bits here
+			irig_bitstream(9'bx10000010);
 
-            // Minutes: 59 = _101_1001
-            irig_bit(MARK); // 09
-            irig_bitstream(9'b010101001);
-            
-            // Hours: 17 = __01_0111
-            irig_bit(MARK); // 19
-            irig_bitstream(9'b000100111);
+			// Minutes: 59 = _101_1001
+			irig_bit(MARK); // 09
+			irig_bitstream(9'b010101001);
+			
+			// Hours: 17 = __01_0111
+			irig_bit(MARK); // 19
+			irig_bitstream(9'b000100111);
 
-            // Day of year 293 (93) = 1001_0011 
-            irig_bit(MARK); // 29
-            irig_bitstream(9'b100100011);
+			// Day of year 293 (93) = 1001_0011 
+			irig_bit(MARK); // 29
+			irig_bitstream(9'b100100011);
 
-            // Day of year 293 (2) = _______10
-            // Tenth of seconds unused
-            irig_bit(MARK); // 39
-            irig_bitstream(9'b000000010);
+			// Day of year 293 (2) = _______10
+			// Tenth of seconds unused
+			irig_bit(MARK); // 39
+			irig_bitstream(9'b000000010);
 
-            // Year 16 = 0001_0110
-            irig_bit(MARK); // 49
-            irig_bitstream(9'b000100110);
+			// Year 16 = 0001_0110
+			irig_bit(MARK); // 49
+			irig_bitstream(9'b000100110);
 
-            // Unused
-            irig_bit(MARK); // 59
-            irig_bitstream(9'b0);
-            irig_bit(MARK); // 69
-            irig_bitstream(9'b0);
+			// Unused
+			irig_bit(MARK); // 59
+			irig_bitstream(9'b0);
+			irig_bit(MARK); // 69
+			irig_bitstream(9'b0);
 
-            // Seconds in day
-            // 17:59:42 = 64782 = _01111110100001110
-            irig_bit(MARK); // 79
-            irig_bitstream(9'b100001110);
-            irig_bit(MARK); // 89
-            irig_bitstream(9'b001111110);
-            irig_bit(MARK); // 99            
-          end
-    endtask // irig_timestamp
+			// Seconds in day
+			// 17:59:42 = 64782 = _01111110100001110
+			irig_bit(MARK); // 79
+			irig_bitstream(9'b100001110);
+			irig_bit(MARK); // 89
+			irig_bitstream(9'b001111110);
+			irig_bit(MARK); // 99			 
+		  end
+	endtask // irig_timestamp
 
-    // Send a stream of IRIG bits
-    // 'x' means skip completely FIX ME?
-    task irig_bitstream;
-        input [8:0] s;
-        begin
-        repeat (9) 
-          begin
-            case (s[0])
-              1'b1:
-                irig_bit(D0);
-              1'b0:
-                irig_bit(D1);
-            endcase // case (s[0])
-            s = s >> 1'b1;
-          end 
-        end
-    endtask
-              
-    // Send a single width-encoded bit
-    task irig_bit;
-        input [2:0] ib; // mark, 1, 0
-        begin
-            @(posedge clk_10mhz);
-            irigb = 1'b1;
-            case (ib)
-              D0: 
-                begin
-                    #2000500 irigb = 1'b0;
-                    #7999500;
-                end
-              D1:
-                begin
-                    #5000500 irigb = 1'b0;
-                    #4999500;
-                end
-              MARK:
-                begin 
-                    #8000500 irigb = 1'b0;
-                    #2999500;
-                end
-            endcase            
-        end
-    endtask
+	// Send a stream of IRIG bits
+	// 'x' means skip completely FIX ME?
+	task irig_bitstream;
+		input [8:0] s;
+		begin
+		repeat (9) 
+		  begin
+			case (s[0])
+			  1'b1:
+				irig_bit(D0);
+			  1'b0:
+				irig_bit(D1);
+			endcase // case (s[0])
+			s = s >> 1'b1;
+		  end 
+		end
+	endtask
+			  
+	// Send a single width-encoded bit
+	task irig_bit;
+		input [2:0] ib; // mark, 1, 0
+		begin
+			@(posedge clk_10mhz);
+			irigb = 1'b1;
+			case (ib)
+			  D0: 
+				begin
+					#2000500 irigb = 1'b0;
+					#7999500;
+				end
+			  D1:
+				begin
+					#5000500 irigb = 1'b0;
+					#4999500;
+				end
+			  MARK:
+				begin 
+					#8000500 irigb = 1'b0;
+					#2999500;
+				end
+			endcase			   
+		end
+	endtask
 
 endmodule
